9df006d686225927601f224a0804bba6fd203e98
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-09-23 18:17:56 -0700

    tests and app running with stretch goal

---
+++ src/main/java/quotes/Quote.java
@@ -0,0 +1,17 @@
+package quotes;
+
+public class Quote {
+    public String author;
+    public String text;
+
+    public Quote(String author, String text) {
+        this.author = author;
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return "By  " + author + '\'' +
+                 text + '\'';
+    }
+}
